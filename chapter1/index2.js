4e3bfe0851fd691e1048b3605608c3075489231c
Abhijat Patel <abhijatpatelfaizabad@gmail.com>
2026-04-14 04:13:36 +0530

    loop statement

---
+++ chapter1/index2.js
@@ -0,0 +1,20 @@
+//loop statement
+
+//for loop
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+
+//while loop
+let y=1;
+while(y<=5){
+    console.log(y);
+    y++;
+}
+
+//do-while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<10);
