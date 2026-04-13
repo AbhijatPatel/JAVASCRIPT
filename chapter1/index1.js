8f20f50b536629aba86d72bc50e21818a341ecdf
Abhijat Patel <abhijatpatelfaizabad@gmail.com>
2026-04-14 04:13:13 +0530

    conditional statement

---
+++ chapter1/index1.js
@@ -0,0 +1,51 @@
+//conditional statement
+
+//if
+let age=18;
+if(age>=18){
+    console.log("adult");
+}
+
+//if-else
+    let aayu=18;
+if(aayu>=18){
+    console.log("adult");
+}
+else{
+    console.log("minor");
+}
+
+//if-else-if
+let marks=88;
+if (marks>90) {
+    console.log("A");
+}
+else if(marks>80) {
+    console.log("B");
+}
+else if(marks>60) {
+    console.log("C");
+}
+else if(marks>60) {
+    console.log("D");
+}
+else{
+    console.log("E")
+}
+
+//switch
+let day=2;
+switch(day){
+    case 1:
+    console.log("monday");
+    break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+        default:
+            console.log("other")
+ 
+}
